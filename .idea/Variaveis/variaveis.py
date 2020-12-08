706ed208286931eccff03bb5d470e9b4b3e749d9
JessicaQueiroz <jehqueiroz01@gmail.com>
2020-12-08 20:15:27 -0300

    Initial commit

---
+++ .idea/Variaveis/variaveis.py
@@ -0,0 +1,13 @@
+nome = "Miguel"
+idade = 26
+altura = 1.80
+e_maior = idade > 18
+peso = 80
+imc = peso / altura**2
+
+print('Nome: ', nome)
+print('Idade: ', idade)
+print('Altura: ', altura)
+print('É maior: ', e_maior)
+
+print(nome, 'tem ', idade, 'anos de idade e seu imc é ',round(imc))
